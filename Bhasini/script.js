a61d64b59a91534c69fb2a5a965a4ca9d7134f0d
Shubh Kesharwani <95687419+drew7wonders@users.noreply.github.com>
2023-11-01 22:12:38 +0530

    Update script.js

--- Bhasini/script.js
+++ Bhasini/script.js
@@ -4,7 +4,9 @@ const output_lang = document.getElementById("slct-out");
 const inp_area = document.getElementById("input-area");
 const output_area = document.getElementById("output-area");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", () => { translate(); });
+
+async function translate() {
     const sourceLanguage = input_lang.value;
     const targetLanguage = output_lang.value;
     const textToTranslate = inp_area.value;
@@ -15,21 +17,14 @@ button.addEventListener("click", () => {
         target: targetLanguage
     };
     console.log(requestData);
-  // ...
 
-    fetch('https://bhashiniproject.up.railway.app/translate', {
+    const response = await fetch('https://bhashiniproject.up.railway.app/translate', {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(requestData),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        document.getElementById("output-area").innerText = data.translated_content;
-    })
-    .catch(error => {
-        console.error(error);
     });
-
-});
+    const data = await response.json();
+    document.getElementById("output-area").innerText = data.translated_content;
+}
